47e0009344b69182a41c2f8d5f9494aae2597184
Pixop <mepixop@gmail.com>
2023-12-16 19:38:13 +0100

    Recipe Header

--- apps/meal-planner/src/app/components/recipe-book/recipe-header/recipe-header.component.ts
+++ apps/meal-planner/src/app/components/recipe-book/recipe-header/recipe-header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'projects-recipe-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './recipe-header.component.html',
   styleUrl: './recipe-header.component.css',
 })
-export class RecipeHeaderComponent {}
+export class RecipeHeaderComponent { }
