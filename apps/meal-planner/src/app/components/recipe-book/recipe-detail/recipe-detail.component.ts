7d89436e69999963cb43a7e9b8f3ad2b0ae1ec54
Pixop <mepixop@gmail.com>
2024-01-04 19:18:54 +0100

    html recipe details

--- apps/meal-planner/src/app/components/recipe-book/recipe-detail/recipe-detail.component.ts
+++ apps/meal-planner/src/app/components/recipe-book/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Recipe } from '../../../libs/models';
+import { RecipeService } from '../../../libs/services/recipe.service';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'projects-recipe-detail',
@@ -8,4 +12,32 @@ import { CommonModule } from '@angular/common';
   templateUrl: './recipe-detail.component.html',
   styleUrl: './recipe-detail.component.css',
 })
-export class RecipeDetailComponent {}
+export class RecipeDetailComponent implements OnInit, OnDestroy {
+  recipe: Recipe | undefined;
+  id: string | undefined;
+  destroy: Subscription[] = [];
+
+  constructor(
+    private recipeService: RecipeService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.id = this.route.snapshot.params['id'];
+  }
+
+  ngOnInit(): void {
+    this.destroy.push(
+      this.route.params.subscribe((params: Params) => {
+        this.recipe = this.recipeService.getRecipeById(params['id']);
+        if (this.recipe === undefined) {
+          this.recipe = new Recipe('', '', '', '', []);
+        }
+        this.id = params['id'];
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.destroy.forEach((s) => s.unsubscribe());
+  }
+}
